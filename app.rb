1db9c0b8650bc0e5970790382e9263953692d091
betahikaru <beta.hikaru2000+github@gmail.com>
2014-08-23 00:11:21 +0900

    Set API Key

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require 'rubygems'
 require 'easy_translate'
 
 begin
-  puts EasyTranslate.translate('Hello, world', :to => :japanese).tosjis
+  puts ENV['TRANSLATE_API_KEY']
+  EasyTranslate.api_key = ENV['TRANSLATE_API_KEY']
+  puts EasyTranslate.translate('Hello, world', :to => :japanese)
   puts EasyTranslate.translate('Rubyは面白い', :from => :japanese, :to => :en)
 rescue => e
   puts e
